de3c87f7c40b1e9a11c87a07abc9fbc9fcab231f
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-22 22:01:37 +0600

    done

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -5,7 +5,7 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
     <div className="md:w-1/3 mb-10">
       <div className="p-8 rounded-xl mx-5 md:ml-5 mb-5 text-[#6047EC] border-2 border-[#6047EC] bg-[rgba(96,71,236,0.1)]">
-        <h3 className="text-xl font-bold">
+        <h3 className="lg:text-2xl font-bold">
           Spent time on read : {readingTime} min
         </h3>
       </div>
